a94ff48595a1756a67582ed4fdc11317cad69baf
Subhajit Sahu <wolfram77@gmail.com>
2023-12-16 12:04:54 +0530

    :bug: with utility files

---
+++ inc/_main.hxx
@@ -0,0 +1,11 @@
+#pragma once
+#include "_debug.hxx"
+#include "_algorithm.hxx"
+#include "_ctypes.hxx"
+#include "_iostream.hxx"
+#include "_queue.hxx"
+#include "_bitset.hxx"
+#include "_string.hxx"
+#ifdef OPENMP
+#include "_openmp.hxx"
+#endif
---
+++ inc/main.hxx
@@ -0,0 +1,9 @@
+#pragma once
+#define BUILD  0  // 5 == BUILD_TRACE
+#define OPENMP 1
+#include "_main.hxx"
+#include "Graph.hxx"
+#include "update.hxx"
+#include "io.hxx"
+#include "duplicate.hxx"
+#include "symmetricize.hxx"
