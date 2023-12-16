3e881fc48057a80f059713d003bec00371dc0759
Subhajit Sahu <wolfram77@gmail.com>
2023-12-16 16:52:40 +0530

    :bug: with install script

--- main.sh
+++ main.sh
@@ -37,19 +37,20 @@ runTool() {
   stdbuf --output=L ./a.out -i "$1" -o "$1.undirected" "$opt2" "$opt3" 2>&1 | tee -a "$out"
 }
 
-
 # Run tool on all graphs
-runTool ~/Data/web-Stanford.mtx    0 0
-runTool ~/Data/indochina-2004.mtx  0 0
-runTool ~/Data/uk-2002.mtx         0 0
-runTool ~/Data/arabic-2005.mtx     0 0
-runTool ~/Data/uk-2005.mtx         0 0
-runTool ~/Data/webbase-2001.mtx    0 0
-runTool ~/Data/it-2004.mtx         0 0
-runTool ~/Data/sk-2005.mtx         0 0
-runTool ~/Data/com-LiveJournal.mtx 0 1
-runTool ~/Data/com-Orkut.mtx       0 1
-runTool ~/Data/asia_osm.mtx        0 1
-runTool ~/Data/europe_osm.mtx      0 1
-runTool ~/Data/kmer_A2a.mtx        0 1
-runTool ~/Data/kmer_V1r.mtx        0 1
+if [[ "$RUN" != "0" ]]; then
+  runTool ~/Data/web-Stanford.mtx    0 0
+  runTool ~/Data/indochina-2004.mtx  0 0
+  runTool ~/Data/uk-2002.mtx         0 0
+  runTool ~/Data/arabic-2005.mtx     0 0
+  runTool ~/Data/uk-2005.mtx         0 0
+  runTool ~/Data/webbase-2001.mtx    0 0
+  runTool ~/Data/it-2004.mtx         0 0
+  runTool ~/Data/sk-2005.mtx         0 0
+  runTool ~/Data/com-LiveJournal.mtx 0 1
+  runTool ~/Data/com-Orkut.mtx       0 1
+  runTool ~/Data/asia_osm.mtx        0 1
+  runTool ~/Data/europe_osm.mtx      0 1
+  runTool ~/Data/kmer_A2a.mtx        0 1
+  runTool ~/Data/kmer_V1r.mtx        0 1
+fi
